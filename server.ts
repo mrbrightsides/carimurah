28afe68b24082942204fa3abdb49a96b52dda3e4
mrbrightsides <khudri@binadarma.ac.id>
2026-05-25 07:58:14 +0700

    edited server

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ import { ObjectId } from "mongodb";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json({ limit: "150mb" }));
